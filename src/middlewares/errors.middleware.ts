6e309dd363972c61f40234cd4fe45e4017d2f110
Bawanthathilan <bawantharathnayaka@gmail.com>
2024-04-19 17:33:32 +0530

    fix issue in error middleware

--- src/middlewares/errors.middleware.ts
+++ src/middlewares/errors.middleware.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '../exceptions/root';
 
-export const errorMiddleware = (error: HttpException, req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const errorMiddleware = (error: HttpException, req: Request, res: Response , next:NextFunction) => {
   res.status(error.statusCode).json({
     message: error.message,
     errorCode: error.errorCode,
     errors: error.errors
-  });
+})
 };
