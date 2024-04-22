dd79f3a849b91f38cff806a33354a6d5ac0a39cf
Bawanthathilan <bawantha@orelit.com>
2024-04-23 01:00:48 +0530

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'; 
 import { PORT } from './secrets';
 import rootRouter from './routes';
 import { PrismaClient } from '@prisma/client';
@@ -6,6 +7,8 @@ import { errorMiddleware } from './middlewares/errors.middleware';
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api', rootRouter);
 
